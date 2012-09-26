a916a9e29e5ae939d8cda491ba12a967a18506de
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-26 10:53:52 -0300

    Initial Commit

---
+++ MenuContextualTests/MenuContextualTests.h
@@ -0,0 +1,13 @@
+//
+//  MenuContextualTests.h
+//  MenuContextualTests
+//
+//  Created by Rafael Brigagão Paulino on 26/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MenuContextualTests : SenTestCase
+
+@end
---
+++ MenuContextualTests/MenuContextualTests.m
@@ -0,0 +1,32 @@
+//
+//  MenuContextualTests.m
+//  MenuContextualTests
+//
+//  Created by Rafael Brigagão Paulino on 26/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "MenuContextualTests.h"
+
+@implementation MenuContextualTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MenuContextualTests");
+}
+
+@end
